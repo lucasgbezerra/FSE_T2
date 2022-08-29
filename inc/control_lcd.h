81b3a607b184e70c00181729f186bd7c51394686
Lucas Bezerra <lucasbsb100@gmail.com>
2022-08-29 19:21:16 -0300

    Add: LCD controller

---
+++ inc/control_lcd.h
@@ -0,0 +1,28 @@
+#ifndef CONTROL_LCD_
+#define CONTROL_LCD_
+
+#include <wiringPiI2C.h>
+#include <wiringPi.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define I2C_ADDR 0x27
+#define LCD_CHR 1
+#define LCD_CMD 0
+#define LINE1 0x80
+#define LINE2 0xC0
+#define LCD_BACKLIGHT 0x08
+#define ENABLE 0b00000100
+
+int lcd_init(void);
+void lcd_byte(int bits, int mode);
+void lcd_toggle_enable(int bits);
+
+void lcd_type_int(int i);
+void lcd_type_float(float myFloat);
+void lcd_go_to_loc(int line);
+void clear_lcd(void);
+void lcd_type_ln(const char *s);
+void lcd_type_char(char val);
+
+#endif
---
+++ src/control_lcd.c
@@ -0,0 +1,90 @@
+#include "control_lcd.h"
+
+int fd = -1; // seen by all subroutines
+
+void lcd_type_float(float myFloat)
+{
+    char buffer[20];
+    sprintf(buffer, "%4.2f", myFloat);
+    lcd_type_ln(buffer);
+}
+
+void lcd_type_int(int i)
+{
+    char array1[20];
+    sprintf(array1, "%d", i);
+    lcd_type_ln(array1);
+}
+
+void clear_lcd(void)
+{
+    lcd_byte(0x01, LCD_CMD);
+    lcd_byte(0x02, LCD_CMD);
+}
+
+void lcd_go_to_loc(int line)
+{
+    lcd_byte(line, LCD_CMD);
+}
+
+void lcd_type_char(char val)
+{
+    lcd_byte(val, LCD_CHR);
+}
+
+// this allows use of any size string
+void lcd_type_ln(const char *s)
+{
+
+    while (*s)
+        lcd_byte(*(s++), LCD_CHR);
+}
+
+void lcd_byte(int bits, int mode)
+{
+
+    // Send byte to data pins
+    //  bits = the data
+    //  mode = 1 for data, 0 for command
+    int bits_high;
+    int bits_low;
+    // uses the two half byte writes to LCD
+    bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT;
+    bits_low = mode | ((bits << 4) & 0xF0) | LCD_BACKLIGHT;
+
+    // High bits
+    wiringPiI2CReadReg8(fd, bits_high);
+    lcd_toggle_enable(bits_high);
+
+    // Low bits
+    wiringPiI2CReadReg8(fd, bits_low);
+    lcd_toggle_enable(bits_low);
+}
+
+void lcd_toggle_enable(int bits)
+{
+    // Toggle enable pin on LCD display
+    delayMicroseconds(500);
+    wiringPiI2CReadReg8(fd, (bits | ENABLE));
+    delayMicroseconds(500);
+    wiringPiI2CReadReg8(fd, (bits & ~ENABLE));
+    delayMicroseconds(500);
+}
+
+int lcd_init()
+{
+    if (wiringPiSetup() == -1)
+        return -1;
+
+    fd = wiringPiI2CSetup(I2C_ADDR);
+
+    lcd_byte(0x33, LCD_CMD);
+    lcd_byte(0x32, LCD_CMD);
+    lcd_byte(0x06, LCD_CMD);
+    lcd_byte(0x0C, LCD_CMD);
+    lcd_byte(0x28, LCD_CMD);
+    lcd_byte(0x01, LCD_CMD);
+    delayMicroseconds(500);
+
+    return fd;
+}
