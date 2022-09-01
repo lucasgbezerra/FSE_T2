350a148057180363cd29a8cc8d805062a78ff93a
Lucas Bezerra <lucasbsb100@gmail.com>
2022-09-01 16:24:14 -0300

    Update Modbus

--- inc/modbus.h
+++ inc/modbus.h
@@ -7,22 +7,21 @@
 #define SOLICITA 0X23
 #define ENVIA 0x16
 
-#define LIGA 0x01
-#define DESLIGA 0x02
-#define INICIA 0x03
-#define CANCELA 0x04
-#define TEMPO_MAIS 0x06
-#define TEMPO_MENOS 0x07
 #define SOLICITA_TEMP_INT 0xC1
 #define SOLICITA_TEMP_REF 0xC2
 #define LE_COMANDO 0xC3
 #define ENVIA_SINAL_CONTROLE 0xD1
 #define ENVIA_SINAL_REF 0xD2
-#define ENVIA_ESTADO 0xD3
-#define MODO_CONTROLE 0xD4
+#define ENVIA_ESTADO_SISTEMA 0xD3
+#define ENVIA_ESTADO_FUNCIONAMENTO 0xD5
+#define ENVIA_TEMPORIZADOR 0xD6
+
+#define FAIL -1
 static const unsigned char matricula[] = {5, 7, 7, 0};
 
-void le_mensagem();
-void escreve_mensagem(unsigned char *mensagem, unsigned char tam_mensagem, unsigned char codigo, unsigned char sub_codigo);
+// void le_mensagem();
+int read_mensage(unsigned char sub_code, void *data);
+int build_mensage(unsigned char *buffer, unsigned char code, unsigned char sub_code, void *data, int size_data);
+void write_mensage(unsigned char sub_code, void *data);
 
 #endif
--- src/modbus.c
+++ src/modbus.c
@@ -7,67 +7,105 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-unsigned char codigo;
+#define BYTE 1
+unsigned char code;
 
-void escreve_mensagem(unsigned char *mensagem, unsigned char tam_mensagem, unsigned char codigo, unsigned char sub_codigo)
+int build_mensage(unsigned char *buffer, unsigned char code, unsigned char sub_code, void *data, int size_data)
 {
 	int idx = 0;
-	unsigned char *buffer = malloc(9);
-
-	// Contruindo mensagem
+	// Contruindo msg
 	buffer[idx++] = ENDERECO;
-	buffer[idx++] = codigo;
-	buffer[idx++] = sub_codigo;
+	buffer[idx++] = code;
+	buffer[idx++] = sub_code;
 	memcpy(&buffer[idx], matricula, sizeof(matricula));
 	idx += sizeof(matricula);
-
-	if (mensagem != NULL)
+	if (data != NULL)
 	{
-
-		printf("%s\n", mensagem);
-		buffer = realloc(buffer, sizeof(char) * tam_mensagem);
-		memcpy(&buffer[idx], mensagem, tam_mensagem);
-		idx += tam_mensagem;
+		if (sub_code == ENVIA_SINAL_CONTROLE || sub_code == ENVIA_TEMPORIZADOR)
+		{
+			printf("%d\n", (*(int *)data));
+		}
+		else if (sub_code == ENVIA_SINAL_REF)
+		{
+			printf("%.2f\n", *((float *)data));
+		}
+		else
+		{
+			printf("%c\n", (*(char *)data));
+		}
+		memcpy(&buffer[idx], data, size_data);
+		idx += size_data;
 	}
 	short crc = calcula_CRC(buffer, idx);
 
 	memcpy(&buffer[idx], &crc, sizeof(short));
 	idx += sizeof(short);
-	
-	// for (int i = 0; i < idx; i++)
-	// 	printf("%2x ", buffer[i]);
-	// printf("\n");
-	
-	write_serial(buffer, idx);
+
+	return idx;
+}
+
+void write_mensage(unsigned char sub_code, void *data)
+{
+	int size = 0;
+	unsigned char *buffer = malloc(13);
+
+	// if (sub_code == SOLICITA_TEMP_INT)
+	// {
+	// 	size = build_mensage(buffer, SOLICITA, sub_code, NULL, 0);
+	// }
+	if (sub_code == ENVIA_SINAL_CONTROLE || sub_code == ENVIA_SINAL_REF || sub_code == ENVIA_TEMPORIZADOR)
+	{
+		size = build_mensage(buffer, ENVIA, sub_code, data, sizeof(int));
+	}
+	else if (sub_code == ENVIA_ESTADO_SISTEMA || sub_code == ENVIA_ESTADO_FUNCIONAMENTO)
+	{
+		size = build_mensage(buffer, ENVIA, sub_code, data, BYTE);
+	}
+
+	printf("Escreve Mensagem: \n");
+	for (int i = 0; i < size; i++)
+		printf("%2x ", buffer[i]);
+	printf("\n");
+
+	write_serial(buffer, size);
 	free(buffer);
 }
 
-void le_mensagem()
+int read_mensage(unsigned char sub_code, void *data)
 {
-	unsigned char *rx_buffer;
 	int rx_length = 0;
+	int tx_length = 0;
 	int size = 9;
 	short crc_recv;
+	unsigned char *rx_buffer = malloc(size);
+	unsigned char *tx_buffer = malloc(13);
 
-	rx_buffer = malloc(size);
+	tx_length = build_mensage(tx_buffer, SOLICITA, sub_code, NULL, 0);
+	write_serial(tx_buffer, tx_length);
+	usleep(500000);
 
 	rx_length = read_serial(rx_buffer, size);
-
-
 	int length = rx_length - sizeof(short);
 	memcpy(&crc_recv, &rx_buffer[length], sizeof(short));
 	short crc = calcula_CRC(rx_buffer, length);
+	;
 
-	if (crc_recv == crc){
-		float temp = 0;
-		memcpy(&temp, &rx_buffer[3], 4);
-		printf("TEMP: %2f\n", temp);
+	printf("Lê Mensagem: \n");
+	for (int i = 0; i < 9; i++)
+		printf("%2x ", rx_buffer[i]);
+	printf("\n");
 
+	if (crc_recv == crc)
+	{
+		memcpy(data, &rx_buffer[3], 4 * BYTE);
+		free(rx_buffer);
+		free(tx_buffer);
+		return rx_length;
 	}
-
-	// for (int i = 0; i < 9; i++)
-	// 	printf("%2x ", rx_buffer[i]);
-	// printf("\n");
-
-	free(rx_buffer);
+	else
+	{
+		free(rx_buffer);
+		free(tx_buffer);
+		return -1;
+	}
 }
