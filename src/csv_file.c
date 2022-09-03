93a57a0fed2ceab37ad4266eec24fd25a59b1441
Lucas <lucasbsb100@gmail.com>
2022-09-03 16:38:30 -0300

    Fix: ignored temperature

--- src/csv_file.c
+++ src/csv_file.c
@@ -11,45 +11,6 @@ void create_file(char *file_name)
     fclose(file);
 }
 
-// void load_file()
-// {
-//     FILE *file = fopen(file_name, "r");
-//     if (!file)
-//     {
-//         printf("Arquivo não existe\n");
-//     }
-
-//     char buffer[1024];
-//     int row = 0;
-//     int column = 0;
-//     while (fgets(buffer, 1024, file))
-//     {
-//         column = 0;
-//         row++;
-
-//         if (row == 1)
-//             continue;
-
-//         char *value = strtok(buffer, ", ");
-//         while (value)
-//         {
-//             if (column == 0)
-//             {
-//                 dados[row - 2].tempo = strtof(value, NULL);
-//             }
-
-//             if (column == 1)
-//             {
-//                 dados[row - 2].temp = strtof(value, NULL);
-//             }
-
-//             value = strtok(NULL, ", ");
-//             column++;
-//         }
-//     }
-//     fclose(file);
-// }
-
 void save_on_file(char *file_name, float ti, float tr, float ta, int signal)
 {
     struct tm  *date;
@@ -67,7 +28,7 @@ void save_on_file(char *file_name, float ti, float tr, float ta, int signal)
 
     fprintf(file, "%d/%d/%d, ", date->tm_mday, date->tm_mon + 1, date->tm_year + 1900);
     fprintf(file, "%d:%d:%d, ", date->tm_hour, date->tm_min, date->tm_sec);
-    fprintf(file, "%.2lf, %.2lf, %d\n", ta, tr, signal);
+    fprintf(file, "%.2lf, %.2lf, %.2lf, %d\n",ti, ta, tr, signal);
 
     fclose(file);
 }
